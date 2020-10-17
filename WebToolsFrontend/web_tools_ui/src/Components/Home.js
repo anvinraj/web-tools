8b3254efa4446bcff981d6d814cd40a80378fdb8
anvinraj <anvin.career@gmail.com>
2020-10-17 13:12:05 +0530

    front end

---
+++ WebToolsFrontend/web_tools_ui/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css_styles/Home.css';
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="body">
+                <div className="heading">Web Tools</div>
+                <div className="mainBox"></div>
+            </div>
+        );
+    }
+}
+
+export default Home;
