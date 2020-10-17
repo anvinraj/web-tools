9bb34094a4c6a33cf3c70ee102e3c59cd8e7db45
anvinraj <anvin.career@gmail.com>
2020-10-17 19:56:39 +0530

    changes

--- WebToolsFrontend/web_tools_ui/src/Components/Home.js
+++ WebToolsFrontend/web_tools_ui/src/Components/Home.js
@@ -3,15 +3,42 @@ import '../css_styles/Home.css';
 
 
 class Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            formBoxVisible:false,
+            imageUploadForm:false,
+            videoUploadForm:false
+        };
+    }
+
+    imageUploadFormFun = () =>{
+        this.setState({
+            formBoxVisible:true,
+            imageUploadForm:true
+        });
+
+    }
+
     render(){
         return(
-            <div className="body">
+            <div className="body">                
                 <div className="heading">Web Tools</div>
+
+                {!this.state.formBoxVisible ?
                 <div className="mainBox">
-                    <a href="#"><div className="item"> Face Detection from Image</div></a>
-                    <a href="#"><div className="item"> Face Detection from Video</div></a>
+                    <a onClick={this.imageUploadFormFun}><div className="item"> Face Detection from Image</div></a>
+                    <a><div className="item"> Face Detection from Video</div></a>
 
                 </div>
+                :
+                <div className="mainBox">
+
+                    <input type="file"/>
+
+                </div>
+                }
             </div>
         );
     }
