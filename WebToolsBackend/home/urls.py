47d13fa68c3e33992e237b88304d65b60edf0437
anvinraj <anvin.career@gmail.com>
2020-10-23 19:35:04 +0530

    changes

--- WebToolsBackend/home/urls.py
+++ WebToolsBackend/home/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from . views import index
+from . views import *
 
 urlpatterns = [
-    path('', index),
+    path('index', index),
+    path('upload_image', uploadImage),
 	
 ]
--- WebToolsBackend/home/views.py
+++ WebToolsBackend/home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
+	return render(request, "index.html") 
+
+@csrf_exempt
+def uploadImage(request):
+	if request.method == 'POST':
+		if 'file' in request.FILES:
+			fileName=request.FILES['file'].name
+			print(fileName)
+
 	return render(request, "index.html") 
