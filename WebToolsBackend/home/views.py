933957e19258dd33ecd2c611abd675d6d938712e
anvinraj <anvin.career@gmail.com>
2020-11-19 21:29:34 +0530

    API update

--- WebToolsBackend/home/views.py
+++ WebToolsBackend/home/views.py
@@ -3,6 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
 import json
 from django.http import HttpResponse
+from PIL import Image
+from PIL.ExifTags import TAGS
+import os.path
+SITE_ROOT = os.path.dirname(os.path.dirname(__file__))
 # Create your views here.
 
 def index(request):
@@ -22,11 +26,31 @@ def uploadImage(request):
 				filename = fs.save(receivedFileName, myfile)
 				file_url = fs.url(filename)
 				response_data['message'] = 'success'
-				response_data['url'] = file_url
+				response_data['url'] = request.META['HTTP_HOST']+ '/uploads' +  file_url
+				imageLocation='uploads'+file_url
+				image = Image.open(imageLocation)
+				exifdata = image.getexif()
+				metadataValue = ''
+				for tag_id in exifdata:
+				    # get the tag name, instead of human unreadable tag id
+				    tag = TAGS.get(tag_id, tag_id)
+				    data = exifdata.get(tag_id)
+				    # decode bytes 
+				    if isinstance(data, bytes):
+				    	try:
+				        	data = data.decode()
+				    	except Exception as e:
+				        	pass
+				    #print(f"{tag:25}: {data}")
+				    metadataValue += f"{tag:25}: {data}" + '\n'
+				response_data['status'] = 'success'
+				response_data['data'] = metadataValue
 				return HttpResponse(json.dumps(response_data), content_type="application/json")
 		else:
+			response_data['status'] = 'failed'
 			response_data['message'] = 'request methode must be POST'
 			return HttpResponse(json.dumps(response_data), content_type="application/json")
 	except Exception as e:
+		response_data['status'] = 'failed'
 		response_data['message'] = str(e)
 		return HttpResponse(json.dumps(response_data), content_type="application/json")
