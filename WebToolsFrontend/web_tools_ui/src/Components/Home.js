401bcc02eda1ce7e39ce6bb047ec7fd83fea35d6
anvinraj <anvin.career@gmail.com>
2021-06-05 19:07:56 +0530

    adding api rsponse in view

--- WebToolsFrontend/web_tools_ui/src/Components/Home.js
+++ WebToolsFrontend/web_tools_ui/src/Components/Home.js
@@ -14,7 +14,8 @@ class Home extends Component{
             videoUploadForm:false,
             selectedFile:null,
             respImgUrl:def_image,
-            response:false
+            response:false,
+            respData:''
         };
     }
 
@@ -49,11 +50,13 @@ class Home extends Component{
               if(response.message=="success"){
                   this.setState({
                     respImgUrl:'http://'+response.url,
-                    response:true
+                    response:true,
+                    respData:response
                   });
               }else{
                   this.setState({
-                    response:false
+                    response:false,
+                    response:''
                   });
               }
 
@@ -84,17 +87,57 @@ class Home extends Component{
                     <div className="mainBox">
                         <div className="fileBox" >
                           <input type="file" onChange={this.onFileChange}/>
-                          <button className="buttonBox" onClick={this.onFileUpload}>Submit</button>
+                          <button className="buttonBox" onClick={this.onFileUpload}>Upload</button>
                           <br/>
-
-                          <div className="contentBox">
-                              <div className="imgBox">
-                                  {this.state.response ?
+                          {this.state.response ?
+                              <div className="contentBox">
+                                  <div className="imgBox">
                                       <img src={this.state.respImgUrl} className="imageStyle"/>
-                                  : null }
+                                  </div>
+                                  <div className="descBox">
+                                      <div className="divRow"><div className="tagSpan">ExifVersion</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExifVersion}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ComponentsConfiguration</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ComponentsConfiguration}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ShutterSpeedValue</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ShutterSpeedValue}</div></div>
+                                      <div className="divRow"><div className="tagSpan">DateTimeOriginal</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.DateTimeOriginal}</div></div>
+                                      <div className="divRow"><div className="tagSpan">DateTimeDigitized</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.DateTimeDigitized}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ApertureValue</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ApertureValue}</div></div>
+                                      <div className="divRow"><div className="tagSpan">BrightnessValue</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.BrightnessValue}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExposureBiasValue</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExposureBiasValue}</div></div>
+                                      <div className="divRow"><div className="tagSpan">MaxApertureValue</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.MaxApertureValue}</div></div>
+                                      <div className="divRow"><div className="tagSpan">MeteringMode</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.MeteringMode}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Flash</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Flash}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ColorSpace</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ColorSpace}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExifImageWidth</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExifImageWidth}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExifInteroperabilityOffset</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExifInteroperabilityOffset}</div></div>
+                                      <div className="divRow"><div className="tagSpan">SceneCaptureType</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.SceneCaptureType}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExifImageHeight</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExifImageHeight}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Contrast</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Contrast}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Saturation</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Saturation}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Sharpness</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Sharpness}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Model</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Model}</div></div>
+                                      <div className="divRow"><div className="tagSpan">YCbCrPositioning</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.YCbCrPositioning}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Make</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Make}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExposureTime</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExposureTime}</div></div>
+                                      <div className="divRow"><div className="tagSpan">XResolution</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.XResolution}</div></div>
+                                      <div className="divRow"><div className="tagSpan">YResolution</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.YResolution}</div></div>
+                                      <div className="divRow"><div className="tagSpan">FNumber</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.FNumber}</div></div>
+                                      <div className="divRow"><div className="tagSpan">SceneType</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.SceneType}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExposureProgram</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExposureProgram}</div></div>
+                                      <div className="divRow"><div className="tagSpan">GPSInfo</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.GPSInfo}</div></div>
+                                      <div className="divRow"><div className="tagSpan">CustomRendered</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.CustomRendered}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ISOSpeedRatings</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ISOSpeedRatings}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ResolutionUnit</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ResolutionUnit}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExposureMode</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExposureMode}</div></div>
+                                      <div className="divRow"><div className="tagSpan">FlashPixVersion</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.FlashPixVersion}</div></div>
+                                      <div className="divRow"><div className="tagSpan">WhiteBalance</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.WhiteBalance}</div></div>
+                                      <div className="divRow"><div className="tagSpan">Software</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.Software}</div></div>
+                                      <div className="divRow"><div className="tagSpan">DateTime</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.DateTime}</div></div>
+                                      <div className="divRow"><div className="tagSpan">DigitalZoomRatio</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.DigitalZoomRatio}</div></div>
+                                      <div className="divRow"><div className="tagSpan">ExifOffset</div><div className="divSep">:</div><div className="valueSpan">{this.state.respData.data.ExifOffset}</div></div>
+
+                                  </div>
                               </div>
-                              <div className="descBox"></div>
-                          </div>
+                          : null }
 
 
                         </div>
